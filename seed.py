520b44c86ac4e8e70c35de01f773769d914d5a47
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 18:03:20 -0700

    loaded users

--- seed.py
+++ seed.py
@@ -3,7 +3,20 @@ import csv
 
 def load_users(session):
     # use u.user
-    pass
+
+    with open('seed_data/u.user', 'rb') as csvfile:
+        all_users = csv.reader(csvfile, delimiter = "|")
+        for line in all_users:
+            u = model.User()
+            u.id = int(line[0])
+            u.age = int(line[1])
+            u.zipcode = line[4]
+            print (u.id, u.age, u.zipcode)
+            session.add(u)
+
+
+
+    
 
 def load_movies(session):
     # use u.item
@@ -15,7 +28,10 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    session.commit()
+
+
 
 if __name__ == "__main__":
     s= model.connect()
