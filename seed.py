8223f84a4127becc30782f5b9cbdd48042fd3d32
Hackbright Student <info@hackbrightacademy.com>
2014-10-24 12:39:47 -0700

    part one done

--- seed.py
+++ seed.py
@@ -1,5 +1,6 @@
 import model
 import csv
+from datetime import datetime
 
 def load_users(session):
     # use u.user
@@ -20,15 +21,36 @@ def load_users(session):
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item', 'rb') as csvfile:
+        all_movies = csv.reader(csvfile, delimiter = "|")
+        for line in all_movies:
+            u = model.Movie()
+            u.id = int(line[0])
+            u.name = line[1].decode("latin-1")
+            if line[2]:
+                u.released_at = datetime.strptime(line[2], "%d-%b-%Y")
+            print(u.released_at)
+            u.imdb_url = line[4].decode("latin-1")
+            session.add(u)
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/u.data', 'rb') as csvfile:
+        all_ratings = csv.reader(csvfile, delimiter= "\t")
+        for line in all_ratings:
+            u = model.Rating()
+            print line[0]
+            u.user_id = int(line[0])
+            u.movie_id = int(line[1])
+            u.rating = int(line[2])
+            session.add(u)
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    load_users(session)
+    # load_users(session)
+    #load_movies(session)
+    load_ratings(session)
     session.commit()
 
 
